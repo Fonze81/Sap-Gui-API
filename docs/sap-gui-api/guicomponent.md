2f7a8f63b808692b289c7109d38c4b94d48cbca6
Fonze81 <Fonze81@outlook.com>
2022-09-10 10:02:04 -0300

    Created basic structure of the class

---
+++ docs/sap-gui-api/guicomponent.md
@@ -0,0 +1,15 @@
+# GuiComponent :simple-sap:
+
+## Atributes
+
+element  
+container_type  
+id  
+name  
+parent  
+type  
+type_as_number
+
+## Methods
+
+validate_element_type()
---
+++ src/sapguiapi.py
@@ -0,0 +1,98 @@
+"""
+SAP GUI Scripting API
+---------------------
+
+DEVELOPER GUIDE | PUBLIC
+Document Version: 7.60 PL1 – 2019-03-28
+https://help.sap.com/doc/9215986e54174174854b0af6bb14305a/760.01/en-US/sap_gui_scripting_api_761.pdf
+"""
+
+# TODO Create class based on the documentation of the 'GuiAbapEditor'
+# TODO Create class based on the documentation of the 'GuiApoGrid'
+# TODO Create class based on the documentation of the 'GuiApplication'
+# TODO Create class based on the documentation of the 'GuiBarChart'
+# TODO Create class based on the documentation of the 'GuiBox'
+# TODO Create class based on the documentation of the 'GuiButton'
+# TODO Create class based on the documentation of the 'GuiCalendar'
+# TODO Create class based on the documentation of the 'GuiChart'
+# TODO Create class based on the documentation of the 'GuiCheckBox'
+# TODO Create class based on the documentation of the 'GuiCollection'
+# TODO Create class based on the documentation of the 'GuiColorSelector'
+# TODO Create class based on the documentation of the 'GuiComboBox'
+# TODO Create class based on the documentation of the 'GuiComboBoxControl'
+# TODO Create class based on the documentation of the 'GuiComboBoxEntry'
+# TODO Create class based on the documentation of the 'GuiComponent'
+
+class GuiComponent():
+
+    #_element = self.element
+    #_container_type = self.container_type
+    #_id = self.id
+    #_name = self.name
+    #_parent = self.parent
+    #_type = self.type
+    #_type_as_number = self.type_as_number
+    #validate_element_type(self)
+
+    def __init__(self):
+        pass
+
+
+# TODO Create class based on the documentation of the 'GuiComponentCollection'
+# TODO Create class based on the documentation of the 'GuiConnection'
+# TODO Create class based on the documentation of the 'GuiContainer'
+# TODO Create class based on the documentation of the 'GuiContainerShell'
+# TODO Create class based on the documentation of the 'GuiContextMenu'
+# TODO Create class based on the documentation of the 'GuiCTextField'
+# TODO Create class based on the documentation of the 'GuiCustomControl'
+# TODO Create class based on the documentation of the 'GuiDialogShell'
+# TODO Create class based on the documentation of the 'GuiEAIViewer2D'
+# TODO Create class based on the documentation of the 'GuiEAIViewer3D'
+# TODO Create class based on the documentation of the 'GuiEnum'
+# TODO Create class based on the documentation of the 'GuiFrameWindow'
+# TODO Create class based on the documentation of the 'GuiGOSShell'
+# TODO Create class based on the documentation of the 'GuiGraphAdapt'
+# TODO Create class based on the documentation of the 'GuiGridView'
+# TODO Create class based on the documentation of the 'GuiHTMLViewer'
+# TODO Create class based on the documentation of the 'GuiInputFieldControl'
+# TODO Create class based on the documentation of the 'GuiLabel'
+# TODO Create class based on the documentation of the 'GuiMainWindow'
+# TODO Create class based on the documentation of the 'GuiMap'
+# TODO Create class based on the documentation of the 'GuiMenu'
+# TODO Create class based on the documentation of the 'GuiMenubar'
+# TODO Create class based on the documentation of the 'GuiMessageWindow'
+# TODO Create class based on the documentation of the 'GuiModalWindow'
+# TODO Create class based on the documentation of the 'GuiNetChart'
+# TODO Create class based on the documentation of the 'GuiOfficeIntegration'
+# TODO Create class based on the documentation of the 'GuiOkCodeField'
+# TODO Create class based on the documentation of the 'GuiPasswordField'
+# TODO Create class based on the documentation of the 'GuiPicture'
+# TODO Create class based on the documentation of the 'GuiRadioButton'
+# TODO Create class based on the documentation of the 'GuiSapChart'
+# TODO Create class based on the documentation of the 'GuiScrollbar'
+# TODO Create class based on the documentation of the 'GuiScrollContainer'
+# TODO Create class based on the documentation of the 'GuiSession'
+# TODO Create class based on the documentation of the 'GuiSessionInfo'
+# TODO Create class based on the documentation of the 'GuiShell'
+# TODO Create class based on the documentation of the 'GuiSimpleContainer'
+# TODO Create class based on the documentation of the 'GuiSplit'
+# TODO Create class based on the documentation of the 'GuiSplitterContainer'
+# TODO Create class based on the documentation of the 'GuiStage'
+# TODO Create class based on the documentation of the 'GuiStatusbar'
+# TODO Create class based on the documentation of the 'GuiStatusPane'
+# TODO Create class based on the documentation of the 'GuiTab'
+# TODO Create class based on the documentation of the 'GuiTableColumn'
+# TODO Create class based on the documentation of the 'GuiTableControl'
+# TODO Create class based on the documentation of the 'GuiTableRow'
+# TODO Create class based on the documentation of the 'GuiTabStrip'
+# TODO Create class based on the documentation of the 'GuiTextedit'
+# TODO Create class based on the documentation of the 'GuiTextField'
+# TODO Create class based on the documentation of the 'GuiTitlebar'
+# TODO Create class based on the documentation of the 'GuiToolbar'
+# TODO Create class based on the documentation of the 'GuiToolbarControl'
+# TODO Create class based on the documentation of the 'GuiTree'
+# TODO Create class based on the documentation of the 'GuiUserArea'
+# TODO Create class based on the documentation of the 'GuiUtils'
+# TODO Create class based on the documentation of the 'GuiVComponent'
+# TODO Create class based on the documentation of the 'GuiVContainer'
+# TODO Create class based on the documentation of the 'GuiVHViewSwitch'
