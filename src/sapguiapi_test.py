fe91756e646cb994a6dbc798109dda7a82dd33dc
Fonze81 <Fonze81@outlook.com>
2022-09-11 09:50:37 -0300

    Create test structure

---
+++ src/sapguiapi_test.py
@@ -0,0 +1,15 @@
+import unittest
+from sapguiapi import GuiComponent
+
+class TestGuiComponent(unittest.TestCase):
+
+    def test_constructor(self):
+        pass
+
+    def test_constructor_with_arguments(self):
+        pass
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
