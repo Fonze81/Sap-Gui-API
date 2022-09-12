0802d71d51be2180b719a498a69e9a1b900e95c9
Fonze81 <Fonze81@outlook.com>
2022-09-11 21:24:48 -0300

    Update sapguiapi_test.py

--- src/sapguiapi_test.py
+++ src/sapguiapi_test.py
@@ -1,15 +1,31 @@
+from argparse import ArgumentError
 import unittest
 from sapguiapi import GuiComponent
 
 class TestGuiComponent(unittest.TestCase):
 
-    def test_constructor(self):
-        pass
+    def test_constructor(self: object):
+        obj = GuiComponent()
+        self.assertEqual(obj._element, None)
 
-    def test_constructor_with_arguments(self):
-        pass
+    def test_constructor_with_arguments(self: object):
+        pass # TODO
 
-    
+    def test_is_valid(self: object):
+        """Check if the validation is correct.
+
+        Assigns the internal variable an object of the wrong type,
+        to check if the validation is correct.
+        """
+        obj = GuiComponent()
+        obj._element = {}
+        self.assertFalse(obj.is_valid)
+
+    def test_validate_element_type(self: object):
+        with self.assertRaises(TypeError):
+            obj = GuiComponent()
+            errorTest = obj.type
+
 
 if __name__ == '__main__':
     unittest.main()
